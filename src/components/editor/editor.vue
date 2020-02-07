2b242ec857027a9cdc4bf7c69a954da38fc19f4d
eboli <eboli@xidibuy.com>
2020-02-07 23:25:30 +0800

    富文本编辑器替换为UEditor

--- src/components/editor/editor.vue
+++ src/components/editor/editor.vue
@@ -1,17 +1,36 @@
 <template>
   <div class="editor-wrapper">
-    <div :id="editorId"></div>
+    <!-- <div :id="editorId"></div> -->
+    <vue-ueditor-wrap v-model="value" :config="myConfig"></vue-ueditor-wrap>
   </div>
 </template>
 
 <script>
 import Editor from 'wangeditor'
+import VueUeditorWrap from 'vue-ueditor-wrap'
 import 'wangeditor/release/wangEditor.min.css'
 import { oneOf } from '@/libs/tools'
 import Util from '@/libs/util';
 
 export default {
   name: 'Editor',
+  data(){
+    return{
+      myConfig: {
+        content:'',
+        // 编辑器不自动被内容撑高
+        autoHeightEnabled: false,
+        // 初始容器高度
+        initialFrameHeight: 240,
+        // 初始容器宽度
+        initialFrameWidth: '100%',
+        // 上传文件接口（这个地址是我为了方便各位体验文件上传功能搭建的临时接口，请勿在生产环境使用！！！）
+        // serverUrl: 'http://35.201.165.105:8000/controller.php',
+        // UEditor 资源文件的存放路径，如果你使用的是 vue-cli 生成的项目，通常不需要设置该选项，vue-ueditor-wrap 会自动处理常见的情况，如果需要特殊配置，参考下方的常见问题2
+        UEDITOR_HOME_URL: '/UEditor/'
+      }
+    }
+  },
   props: {
     value: {
       type: String,
@@ -42,9 +61,17 @@ export default {
       default: true
     }
   },
+  components: {
+    VueUeditorWrap
+  },
   computed: {
     editorId () {
       return `editor${this._uid}`
+    }
+  },
+  watch: {
+    value (val) {
+      this.setHtml(val)
     }
   },
   methods: {
@@ -55,7 +82,9 @@ export default {
       return this.editor.txt.text()
     },
     setHtml (val) {
-      this.editor.txt.html(val)
+      // this.editor.txt.html(val)
+      this.$emit('input', val)
+      this.$emit('on-change', val)
     }
   },
   mounted () {
