c1400e62d02821448fb650e13d94791e3044ea74
eboli <eboli@xidibuy.com>
2020-02-11 16:14:45 +0800

    富文本编辑器完成图片上传

--- src/plugin/components/editor/editor.vue
+++ src/plugin/components/editor/editor.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="editor-wrapper">
     <!-- <div :id="editorId" ></div> -->
-    <vue-ueditor-wrap v-model="value"  :config="myConfig"></vue-ueditor-wrap>
+    <vue-neditor-wrap v-model="value" :config="myConfig" :destroy="false"></vue-neditor-wrap>
   </div>
 </template>
 
 <script>
 import Editor from 'wangeditor'
 import 'wangeditor/release/wangEditor.min.css'
-import VueUeditorWrap from 'vue-ueditor-wrap'
 import Util from '../../libs/util'
+import VueNeditorWrap from 'vue-neditor-wrap'
+
 export default {
   name: 'Editor',
   data(){
@@ -23,9 +24,9 @@ export default {
         // 初始容器宽度
         initialFrameWidth: '100%',
         // 上传文件接口（这个地址是我为了方便各位体验文件上传功能搭建的临时接口，请勿在生产环境使用！！！）
-        // serverUrl: 'http://35.201.165.105:8000/controller.php',
+        serverUrl: '/adminapi/myUpload',
         // UEditor 资源文件的存放路径，如果你使用的是 vue-cli 生成的项目，通常不需要设置该选项，vue-ueditor-wrap 会自动处理常见的情况，如果需要特殊配置，参考下方的常见问题2
-        UEDITOR_HOME_URL: '/UEditor/'
+        UEDITOR_HOME_URL: '/NEditor/'
       }
     }
   },
@@ -68,7 +69,7 @@ export default {
     }
   },
   components: {
-    VueUeditorWrap
+    VueNeditorWrap
   },
   computed: {
     editorId () {
